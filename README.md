00a4d818324311ce3b10f632c96c1172695c8f6d
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2016-03-22 11:44:27 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Liquid video
+
+Super easy script to make your `iframe` or `embed` responsive / liquid. To run it edit this hunk of code by passing your query.
+
+```
+var elms = document.querySelectorAll('.post iframe');
+makeThisShitResponsive(elms);
+```
+
+[Example on Codepen](http://codepen.io/pawelgrzybek/pen/vGxqaq)
---
+++ liquid-video.js
@@ -0,0 +1,29 @@
+// IIFE to create seperated scope
+(function() {
+  function makeThisShitResponsive(elm) {
+    // loop through all videos
+    for (var i = 0; i < elm.length; i++) {
+      var videoWidth = elm[i].offsetWidth;
+      var videoHeight = elm[i].offsetHeight;
+      var videoRatio = videoHeight / videoWidth * 100 + '%';
+
+      // create wraper per video and add some styles
+      var videoWrapper = document.createElement('div');
+      videoWrapper.style.position = 'relative';
+      videoWrapper.style.paddingBottom = videoRatio;
+
+      // insert each video inside new wrapper
+      elm[i].parentNode.insertBefore(videoWrapper, elm[i]);
+      videoWrapper.appendChild(elm[i]);
+
+      // and add some styling to video
+      elm[i].style.position = 'absolute';
+      elm[i].style.width = '100%';
+      elm[i].style.height = '100%';
+    }
+  }
+
+  // invoke function on custom query
+  var elms = document.querySelectorAll('.post iframe');
+  makeThisShitResponsive(elms);
+})();
